d01dc1b3914f5838e2140d53d7ab437d09b8f0b1
SuReaper <79761909+SuReaper@users.noreply.github.com>
2025-11-14 14:09:44 +0000

    Updated Project

--- src/tools/test.js
+++
@@ -1,8 +0,0 @@
-const tokenAddress = "0xF1f4F43191fe903d156b0819E400aA2206Ec998f";
-const url = `https://api.dexscreener.com/latest/dex/search?q=${tokenAddress}`;
-
-const response = await fetch(url);
-const data = await response.json();
-
-const pair = data.pairs[0];
-console.log(JSON.stringify(pair, null, 2)); // full nested object
